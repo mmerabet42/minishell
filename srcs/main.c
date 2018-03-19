3395c2e8549faa1f56ff60de6fdd63fd77bc52d3
Mohammed MERABET <mmerabet@e3r12p18.42.fr>
2018-03-19 22:14:17 +0100

    minishell

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mmerabet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/01 18:40:09 by mmerabet          #+#    #+#             */
-/*   Updated: 2018/03/19 21:02:49 by mmerabet         ###   ########.fr       */
+/*   Updated: 2018/03/19 22:13:56 by mmerabet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -128,7 +128,7 @@ int	main(int argc, char **argv, char **envp)
 		ft_getcursor(&x, NULL);
 		if (x > 1)
 			ft_printf("%#{lgrey}%{bold/;0;0;0}%%%{0}\n");
-		ft_printf("%S %{lred}%s %{lcyan}%s%{0} %{bold}%S%{0} ", L"㋜", g_shell->user, g_shell->pwd, L"∴");
+		ft_printf("%{0}%S %{lred}%s %{lcyan}%s%{0} %{bold}%S%{0} ", L"㋜", g_shell->user, g_shell->pwd, L"∴");
 		ft_strclr(line);
 		if ((c = ft_readraw(line, 2048)) != 3 && c != 4 && line[0] != '\0')
 		{
